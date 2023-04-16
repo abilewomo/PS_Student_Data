a088ff6fbf5f9436b692f491f59b358324076d5f
abilewomo <abilewomo@gmail.com>
2023-04-16 02:03:31 -0400

    first commit

---
+++ src/components/Scores.js
@@ -0,0 +1,17 @@
+export default function Scores(props){
+    //Using map to iterate over each object, the score propert is diaplayed for each date property
+    const score = props.scores.map((score, index) => {
+        return(
+            <div key={index}>
+                Date: {score.date}<br/>
+                Score: {score.score}
+            </div>
+        )
+    })
+    
+    return (
+        <div>
+            {score}
+        </div>
+    )
+}
---
+++ src/components/Students.js
@@ -0,0 +1,14 @@
+import Scores from "./Scores";
+export default function Students(props) {
+     //The name and bio properties are displayed while scores is passed to the Scores component
+        return (
+            <div>
+              <h1>{props.student.name}</h1>
+                <p>{props.student.bio}</p>
+                 <Scores scores ={props.student.scores}/>
+            </div>
+          );
+       
+  }
+
+  
